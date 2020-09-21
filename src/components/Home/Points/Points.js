5209b6fbf9a4d0f3a49a239d37620e4665519f5b
aveWorld <texnarb228@gmail.com>
2020-09-21 11:29:05 +0300

    made smaller text under home points

--- src/components/Home/Points/Points.js
+++ src/components/Home/Points/Points.js
@@ -11,10 +11,10 @@ const Points = () => (
   <section className="points">
     <div className="points__wrapper container">
       <div className="points__list">
-        <PointsItem img={icone1} title="IoT" amount="152,000" />
-        <PointsItem img={icone2} title="Market Increase" subtitle="Billion" amount="$35,4" />
-        <PointsItem img={icone3} title="Security" subtitle="Million" amount="$630" />
-        <PointsItem img={icone4} title="Logistics" subtitle="Million " amount="$630" />
+        <PointsItem img={icone1} title="IoT" subtitle="152,200 IoT devices connected every minute by 2025.” (IDC Data)" />
+        <PointsItem img={icone2} title="Market Increase" subtitle="The global IoT security market is expected to grow from $8.2 billion US dollars in 2018, to $35.4 billion US dollars in 2023. With a CAGR of 33.7% during this period. (Marketwatch)"  />
+        <PointsItem img={icone3} title="Security" subtitle="By 2021, “Industry analysts predict that spending on IoT endpoint security solutions will be more than $630 million.” (Deloitte)" />
+        <PointsItem img={icone4} title="Logistics" subtitle="By 2026, the global connected logistics market size is projected to reach a market capitalization of  USD 82.14 billion. (Global News)" />
       </div>
     </div>
   </section>
