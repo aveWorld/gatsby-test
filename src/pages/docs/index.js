c3e6da205ba4b8d56591b788a89a460ae964cc91
aveWorld <texnarb228@gmail.com>
2020-10-15 11:34:46 +0300

    add 2 pdfs

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -10,20 +10,34 @@ import DevSolutions1 from '../../pdf/Smart Logistics Solutions.pdf';
 import DevSolutions2 from '../../pdf/Organ Transplant Solutions.pdf';
 import DevSolutions3 from '../../pdf/Artifact Preservation.pdf';
 import DevSolutions4 from '../../pdf/Luxury Management.pdf';
+import WhitePaper from '../../pdf/EmeraldCircuitWP.pdf';
+import Tokenomics from '../../pdf/EC Tokenomics2.pdf';
 
 const Docs = () => (
   <Layout>
     <div className="container layout docs-layout">
       <h1 className="docs__h1">
         {' '}
-        <span className="docs__h1__span">/</span>
-        {' '}
-        Start Here To Learn About The Future of IoT
+        <span className="docs__h1__span">/</span> Start Here To Learn About The
+        Future of IoT
       </h1>
       <div className="docs__cards">
-        <DocsCard text="Emerald Circuit Whitepaper" img={docsImg} />
-        <DocsCard text="Emerald Circuit Crypto-Economics Paper" img={docsImg} file={Cryptonomics}/>
+        <DocsCard
+          text="Emerald Circuit Whitepaper"
+          img={docsImg}
+          file={WhitePaper}
+        />
+        <DocsCard
+          text="Emerald Circuit Crypto-Economics Paper"
+          img={docsImg}
+          file={Cryptonomics}
+        />
         <DocsCard text="Team Roadmap" img={docsImg} />
+        <DocsCard
+          text="Emerald Circuit Tokenomics"
+          img={docsImg}
+          file={Tokenomics}
+        />
       </div>
       <h2 className="docs__h2">Product Specifications:</h2>
       <div className="docs__cards">
@@ -33,10 +47,22 @@ const Docs = () => (
       </div>
       <h2>Business Solutions:</h2>
       <div className="docs__cards">
-        <DocsCard text="Smart Logistics Solutions" img={docsImg} file={DevSolutions1} />
-        <DocsCard text="Organ Transplant Solutions" img={docsImg} file={DevSolutions2} />
-        <DocsCard text="Artifact Preservation and
-          Monitoring Solutions" img={docsImg} file={DevSolutions3} />
+        <DocsCard
+          text="Smart Logistics Solutions"
+          img={docsImg}
+          file={DevSolutions1}
+        />
+        <DocsCard
+          text="Organ Transplant Solutions"
+          img={docsImg}
+          file={DevSolutions2}
+        />
+        <DocsCard
+          text="Artifact Preservation and
+          Monitoring Solutions"
+          img={docsImg}
+          file={DevSolutions3}
+        />
         <DocsCard text="Luxury Management" img={docsImg} file={DevSolutions4} />
       </div>
     </div>
